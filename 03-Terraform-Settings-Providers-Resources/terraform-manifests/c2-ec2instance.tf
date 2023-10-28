3a551475c9a6115cf45dda8f6aab65984c6efc0f
Joshua Hublar <joshua.hublar@karglobal.com>
2023-10-28 04:20:01 -0400

    changes for 03.

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,6 +1,9 @@
 # Resource: EC2 Instance
+variable "ami" {
+  default = "ami-0742b4e673072066f"
+}
 resource "aws_instance" "myec2vm" {
-  ami = "ami-0742b4e673072066f"
+  ami = var.ami
   instance_type = "t3.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
